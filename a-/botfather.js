0b379aff1dea80bd23f2cd8297fef3a639d6099b
glebxdlolreal <nepexothnk@gmail.com>
2026-04-28 13:50:00 +0000

    auto: updated resources (test) — обновлены: a-/botfather.js

--- a-/botfather.js
+++ a-/botfather.js
@@ -2441,10 +2441,6 @@ var BotLibrary = {
     }
 
     if (!isNew) {
-      $(document).on('click.curPage', '.copy-btn', function() {
-        navigator.clipboard.writeText(this.dataset.value);
-        Main.showSuccessToast(l('WEB_GENERIC_COPY_SUCCESS'));
-      });
       $(document).on('click.curPage', '.js-editor-delete', function() {
         WebApp.showPopup({
           title: uncleanHTML(l('WEB_LIBRARY_DELETE_CONFIRM_TITLE')),
